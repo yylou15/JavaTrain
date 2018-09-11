1a3421984b9b71805bceba59b6ef9cef9bed4506
Inno_Gear <sunss1903@163.com>
2018-09-11 18:19:54 +0800

    sun

--- src/Utils/BookStatusText.java
+++ src/Utils/BookStatusText.java
@@ -5,25 +5,26 @@ import global.GlobalConst;
 
 public class BookStatusText {
     public static String getBookStatusTxt(BookStatus bookStatus) {
-        switch (bookStatus) {
-            case FREE:
-                return GlobalConst.BOOKSTATUS_FREE;
-            case NONE:
-                return GlobalConst.BOOKSTATUS_NONE;
-            case BORROWED:
-                return GlobalConst.BOOKSTATUS_BORROWED;
-            case REQUESTING:
-                return GlobalConst.BOOKSTATUS_REQUESTING;
-            case BORROWING:
-                return GlobalConst.BOOKSTATUS_BORROWING;
-            case DENIED:
-                return GlobalConst.BOOKSTATUS_DENYED;
-            case REQUESTED:
-                return GlobalConst.BOOKSTATUS_REQUESTED;
-            case LENDING:
-                return GlobalConst.BOOKSTATUS_LENDING;
-            default:
-                return GlobalConst.BOOKSTATUS_FREE;
-        }
+        return "";
+//        switch (bookStatus) {
+//            case FREE:
+//                return GlobalConst.BOOKSTATUS_FREE;
+//            case NONE:
+//                return GlobalConst.BOOKSTATUS_NONE;
+//            case BORROWED:
+//                return GlobalConst.BOOKSTATUS_BORROWED;
+//            case REQUESTING:
+//                return GlobalConst.BOOKSTATUS_REQUESTING;
+//            case BORROWING:
+//                return GlobalConst.BOOKSTATUS_BORROWING;
+//            case DENIED:
+//                return GlobalConst.BOOKSTATUS_DENYED;
+//            case REQUESTED:
+//                return GlobalConst.BOOKSTATUS_REQUESTED;
+//            case LENDING:
+//                return GlobalConst.BOOKSTATUS_LENDING;
+//            default:
+//                return GlobalConst.BOOKSTATUS_FREE;
+//        }
     }
 }
